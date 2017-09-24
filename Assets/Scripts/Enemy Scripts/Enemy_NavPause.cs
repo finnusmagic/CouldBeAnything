ba89b450e0e509982b5258abe50faf4953b57c4f
Finn Pinkenburg <finnpinkenburg@googlemail.com>
2017-09-24 22:11:20 +0200

    Enemy NavMeshAgent Pause if getting Attacked

---
+++ Assets/Scripts/Enemy Scripts/Enemy_NavPause.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy_NavPause : MonoBehaviour {
+
+    Enemy_Master enemyMaster;
+    NavMeshAgent myNavMeshAgent;
+    float pauseTime = 1f;
+
+	void OnEnable() 
+	{
+        SetInitialReferences();
+        enemyMaster.EventEnemyDie += DisableThis;
+        enemyMaster.EventEnemyDeductHealth += PauseNavMeshAgent;
+	}
+
+	void OnDisable() 
+	{
+        enemyMaster.EventEnemyDie -= DisableThis;
+        enemyMaster.EventEnemyDeductHealth -= PauseNavMeshAgent;
+    }
+
+	void SetInitialReferences() 
+	{
+        enemyMaster = GetComponent<Enemy_Master>();
+
+        if (GetComponent<NavMeshAgent>() != null)
+        {
+            myNavMeshAgent = GetComponent<NavMeshAgent>();
+        }
+    }
+
+    void PauseNavMeshAgent(int dummy)
+    {
+        if (myNavMeshAgent != null)
+        {
+            if (myNavMeshAgent.enabled)
+            {
+                myNavMeshAgent.ResetPath();
+                enemyMaster.isNavPaused = true;
+                StartCoroutine(RestartNavMeshAgent());
+            }
+        }
+    }
+
+    IEnumerator RestartNavMeshAgent()
+    {
+        yield return new WaitForSeconds(pauseTime);
+        enemyMaster.isNavPaused = false;
+    }
+
+    void DisableThis()
+    {
+        StopAllCoroutines();
+    }
+}
