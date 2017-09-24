ed040ce4dd386de854059e2c643f4a6ec0ba87d8
Finn Pinkenburg <finnpinkenburg@googlemail.com>
2017-09-24 21:23:02 +0200

    Enemy Master / Enemy Animation / Enemy Detection

---
+++ Assets/Scripts/Enemy Scripts/Enemy_Animation.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Animation : MonoBehaviour {
+
+    Enemy_Master enemyMaster;
+    Animator myAnimator;
+
+
+	void OnEnable() 
+	{
+        SetInitialReferences();
+        enemyMaster.EventEnemyDie += DisableAnimator;
+        enemyMaster.EventEnemyWalking += SetAnimationToWalk;
+        enemyMaster.EventEnemyReachedNavTarget += SetAnimationToIdle;
+        enemyMaster.EventEnemyAttack += SetAnimationToAttack;
+        enemyMaster.EventEnemyDeductHealth += SetAnimationToStruck;
+    }
+
+	void OnDisable() 
+	{
+        enemyMaster.EventEnemyDie -= DisableAnimator;
+        enemyMaster.EventEnemyWalking -= SetAnimationToWalk;
+        enemyMaster.EventEnemyReachedNavTarget -= SetAnimationToIdle;
+        enemyMaster.EventEnemyAttack -= SetAnimationToAttack;
+        enemyMaster.EventEnemyDeductHealth -= SetAnimationToStruck;
+    }
+
+	void SetInitialReferences() 
+	{
+        enemyMaster = GetComponent<Enemy_Master>();
+        if (GetComponent<Animator>() != null)
+        {
+            myAnimator = GetComponent<Animator>();
+        }
+    }
+
+    void SetAnimationToWalk()
+    {
+        if (myAnimator != null)
+        {
+            if (myAnimator.enabled)
+            {
+                myAnimator.SetBool("isPursuing", true);
+            }
+        }
+    }
+
+    void SetAnimationToIdle()
+    {
+        if (myAnimator != null)
+        {
+            if (myAnimator.enabled)
+            {
+                myAnimator.SetBool("isPursuing", false);
+            }
+        }
+    }
+
+    void SetAnimationToAttack()
+    {
+        if (myAnimator != null)
+        {
+            if (myAnimator.enabled)
+            {
+                myAnimator.SetTrigger("Attack");
+            }
+        }
+    }
+
+    void SetAnimationToStruck(int dummy)
+    {
+        if (myAnimator != null)
+        {
+            if (myAnimator.enabled)
+            {
+                myAnimator.SetTrigger("Struck");
+            }
+        }
+    }
+
+    void DisableAnimator()
+    {
+        if (myAnimator != null)
+        {
+            myAnimator.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy Scripts/Enemy_Detection.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Detection : MonoBehaviour {
+
+    Enemy_Master enemyMaster;
+    Transform myTransform;
+
+    public Transform head;
+    public LayerMask playerLayer;
+    public LayerMask sightLayer;
+
+    float checkRate;
+    float nextCheck;
+    float detectRadius = 80f;
+    RaycastHit hit;
+
+	void OnEnable() 
+	{
+        SetInitialReferences();
+        enemyMaster.EventEnemyDie += DisableThis;
+	}
+
+	void OnDisable() 
+	{
+        enemyMaster.EventEnemyDie -= DisableThis;
+    }
+
+    void Update()
+    {
+        CarryOutDetection();
+    }
+
+	void SetInitialReferences() 
+	{
+        enemyMaster = GetComponent<Enemy_Master>();
+        myTransform = transform;
+
+        if (head == null)
+        {
+            head = myTransform;
+        }
+
+        checkRate = Random.Range(0.8f, 1.2f);
+	}
+
+    void CarryOutDetection()
+    {
+        if (Time.time > nextCheck)
+        {
+            nextCheck = Time.time + checkRate;
+
+            Collider[] colliders = Physics.OverlapSphere(myTransform.position, detectRadius, playerLayer);
+
+            if (colliders.Length > 0)
+            {
+                foreach (Collider potentialTargetCollider in colliders)
+                {
+                    if (potentialTargetCollider.CompareTag(GameManager_References._playerTag))
+                    {
+                        if (CanPotentialTargetBeenSeen(potentialTargetCollider.transform))
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                enemyMaster.CallEventEnemyLostTarget();
+            }
+        }
+    }
+
+    void DisableThis()
+    {
+        this.enabled = false;
+    }
+
+    bool CanPotentialTargetBeenSeen(Transform potentialTarget)
+    {
+        if (Physics.Linecast(head.position, potentialTarget.position, out hit, sightLayer))
+        {
+            if (hit.transform == potentialTarget)
+            {
+                enemyMaster.CallEventEnemySetNavTarget(potentialTarget);
+                return true;
+            }
+            else
+            {
+                enemyMaster.CallEventEnemyLostTarget();
+                return false;
+            }
+        }
+        else
+        {
+            enemyMaster.CallEventEnemyLostTarget();
+            return false;
+        }
+    }
+}
