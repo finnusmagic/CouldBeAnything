441e89389432ee28b72837f495c3e5f64d6a4332
Finn Pinkenburg <finnpinkenburg@googlemail.com>
2017-09-24 02:13:42 +0200

    GameManager Setup

---
+++ Assets/Scripts/GameManager Scripts/GameManager_ToggleMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_ToggleMenu : MonoBehaviour {
+
+    GameManagerMaster gameManagerMaster;
+    public GameObject menu;
+
+    void Start()
+    {
+        ToggleMenu();
+    }
+
+    void Update()
+    {
+        CheckForMenuToggleRequest();
+    }
+
+    void OnEnable()
+    {
+        SetInitialReferences();
+        gameManagerMaster.GameOverEvent += ToggleMenu;
+    }
+
+    void OnDisable()
+    {
+        gameManagerMaster.GameOverEvent -= ToggleMenu;
+    }
+
+    void SetInitialReferences()
+    {
+        gameManagerMaster = GetComponent<GameManagerMaster>();
+    }
+
+    void CheckForMenuToggleRequest()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape) && !gameManagerMaster.isGameOver && !gameManagerMaster.isInventoryUIOn)
+        {
+            ToggleMenu();
+        }
+    }
+
+    void ToggleMenu()
+    {
+        if (menu != null)
+        {
+            menu.SetActive(!menu.activeSelf);
+            gameManagerMaster.isMenuOn = !gameManagerMaster.isMenuOn;
+            gameManagerMaster.CallEventMenuToggle();
+        }
+        else
+        {
+            Debug.LogWarning("You need to assing a UI-Gameobject to the Toggle Menu Script.");
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager Scripts/GameManager_TogglePause.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_TogglePause : MonoBehaviour {
+
+    GameManagerMaster gameManagerMaster;
+
+    bool isPaused;
+
+	void OnEnable()
+    {
+        SetInitialReferences();
+        gameManagerMaster.MenuToggleEvent += TogglePause;
+        gameManagerMaster.InventoryUIToggleEvent += TogglePause;
+    }
+
+    void OnDisable()
+    {
+        gameManagerMaster.MenuToggleEvent -= TogglePause;
+        gameManagerMaster.InventoryUIToggleEvent -= TogglePause;
+    }
+
+    void SetInitialReferences()
+    {
+        gameManagerMaster = GetComponent<GameManagerMaster>();
+    }
+
+    void TogglePause()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+    }
+}
